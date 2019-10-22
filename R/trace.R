1562ca3e96694e9ba82ba572e1823f0c1cd968b3
Luiza <luiza.piancastelli@ucdconnect.ie>
2019-10-22 13:35:54 +0100

    deleting

--- R/trace.R
+++
@@ -1,9 +0,0 @@
-#' Function to calculate the trace of a matrix.
-#'
-#' @param matrix Matrix to calculate trace from.
-#'
-#' @return Numeric. Trace value.
-#'
-trace = function(matrix){
-  sum(diag(matrix))
-}
